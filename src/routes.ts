20c51fa538c5390d337f6a957c02abf51c5dc633
Willian Pinheiro <willpinhaaa@gmail.com>
2024-05-15 12:15:24 -0300

    add dockerfile

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,7 @@
+/**
+ * This is where you define your routes and Swagger documentation with JSDoc comments
+ */
+
 import express from "express";
 
 // Controllers
@@ -13,8 +17,21 @@ import fooMiddleware from "@/middlewares/foo.middleware";
 
 const router = express.Router();
 
+// Swagger documentation
 router.get("/docs/swagger.json", (req, res) => res.json(swaggerSpec));
 router.use("/docs", swaggerServe, swaggerSetup);
+
+/**
+ * @swagger
+ * /api/hello:
+ *   get:
+ *     description: Get a hello (hey/hi) message from the sender to the receiver
+ *     responses:
+ *       200:
+ *         description: Returns the hello message
+ *       400:
+ *         description: Invalid request body
+ */
 router.get("/hello", fooMiddleware, helloController);
 
 export default router;
